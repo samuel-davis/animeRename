9a82e1b09b6e9bb34d5114c42f7bf0943ff7c210
samuel.davis <NIKben1020!!!111>
2019-10-06 12:52:48 -0400

    Starting up.

---
+++ anime/src/main/java/davis/anime/renamer/Main.java
@@ -0,0 +1,12 @@
+package davis.anime.renamer;
+
+/**
+ * Copyright(c) 2019
+ * All Rights Reserved.
+ * This software was created by Samuel Davis on 10/6/2019
+ **/
+public class Main {
+
+
+
+}
---
+++ anime/src/main/java/davis/anime/renamer/files/DirectoryScanner.java
@@ -0,0 +1,14 @@
+package davis.anime.renamer.files;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Copyright(c) 2019
+ * All Rights Reserved.
+ * This software was created by Samuel Davis on 10/6/2019
+ **/
+@Slf4j
+public class DirectoryScanner {
+
+
+}
